3dc77b7459fa616f7f0187d408a1526d683702cf
Moony_Yoon <GodmoonGoodman@gmail.com>
2020-09-22 22:31:06 +0900

    dev server

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function App() {
+  console.log(process.env)
   return (
     <div>
       <span>This is App.tsx</span>
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,36 @@
+const path = require('path')
 const appIndex = path.resolve(__dirname, 'src', 'index.tsx')
 
 const appBuild = path.resolve(__dirname, 'build')
 
+const appPublic = path.resolve(__dirname, 'public')
+
+const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false'
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const appHtml = path.resolve(__dirname, 'public', 'index.html')
+
+require('dotenv').config()
+const webpack = require('webpack')
+
+function getClientEnv(nodeEnv) {
+  return {
+    'process.env': JSON.stringify(
+      Object.keys(process.env)
+        .filter((key) => /^REACT_APP/i.test(key))
+        .reduce(
+          (env, key) => {
+            env[key] = process.env[key]
+            return env
+          },
+          { NODE_ENV: nodeEnv }
+        )
+    ),
+  }
+}
+
 module.exports = (webpackEnv) => {
+  const clientEnv = getClientEnv(webpackEnv)
   const isEnvDevelopment = webpackEnv === 'development'
   const isEnvProduction = webpackEnv === 'production'
   return {
@@ -14,5 +42,40 @@ module.exports = (webpackEnv) => {
         ? 'static/js/[name].[contenthash:8].js'
         : isEnvDevelopment && 'static/js/bundle.js',
     },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'ts-loader',
+              options: {
+                transpileOnly: isEnvDevelopment ? true : false,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: appHtml }),
+      new webpack.DefinePlugin(clientEnv),
+    ],
+    devServer: {
+      port: 3000,
+      contentBase: appPublic,
+      open: true,
+      historyApiFallback: true,
+      overlay: true,
+    },
+    devtool: isEnvProduction
+      ? shouldUseSourceMap
+        ? 'source-map'
+        : false
+      : isEnvDevelopment && 'cheap-module-source-map',
   }
 }
